2e7a456333a6ebfedff1dc57b27710d75cb23173
nascvi <vaiiho.nascimbene@gmail.com>
2020-12-16 06:01:58 +0100

    populating with dynamic content��

--- app.js
+++ app.js
@@ -5,11 +5,36 @@ const bodyParser = require('body-parser')
 
 const app = express()
 
+let items = ["buy food", "cook", "eat"]
+
+app.set('view engine', 'ejs')
+
+app.use(bodyParser.urlencoded({extended: true}))
+
 app.get('/', function(req, res) {
-  res.send('Hello')
+  let today = new Date()
+
+  let options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  }
+  
+  let day = today.toLocaleDateString("en-US", options)
+
+  res.render("list", {
+    kindOfDay: day,
+    newListItems: items
+  })
+  
 })
 
+app.post('/', function(req, res) {
+  let item = req.body.newItem
+  items.push(item)
 
+  res.redirect("/")
+})
 
 app.listen(3000, function() {
   console.log('Server started on port 3000')
